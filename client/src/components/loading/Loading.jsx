41d1a94c56e53b17f8d65a5c58a9e0c2b670beaa
Sahil Atahar <sahilatahar@email.com>
2024-02-08 08:26:07 +0530

    Added reload button to loading page

--- client/src/components/loading/Loading.jsx
+++ client/src/components/loading/Loading.jsx
@@ -7,7 +7,13 @@ function Loading({ isError }) {
     const navigate = useNavigate()
     const username = location?.state?.username || ""
 
-    const [message, setMessage] = useState(`Joining the room as ${username}...`)
+    const [message, setMessage] = useState(
+        `Joining the room as ${username}...\nIf it takes too long, please refresh the page`,
+    )
+
+    const reloadPage = () => {
+        window.location.reload()
+    }
 
     const gotoHomePage = () => {
         navigate("/")
@@ -15,12 +21,12 @@ function Loading({ isError }) {
 
     useEffect(() => {
         if (isError) {
-            setMessage("Oops! Something went wrong. Please try again later.")
+            setMessage("Oops! Something went wrong. Please try again")
         }
     }, [isError])
 
     return (
-        <div className="flex h-screen min-h-screen flex-col items-center justify-center gap-6">
+        <div className="flex h-screen min-h-screen flex-col items-center justify-center gap-6 px-4 text-center">
             {!isError && (
                 <div className="flex items-center justify-center space-x-2">
                     <span className="sr-only">Loading...</span>
@@ -29,16 +35,24 @@ function Loading({ isError }) {
                     <div className="h-4 w-4 animate-bounce rounded-full bg-slate-300"></div>
                 </div>
             )}
-            <span className="text-lg font-medium text-slate-300">
+            <span className="whitespace-break-spaces text-lg font-medium text-slate-300">
                 {message}
             </span>
             {isError && (
-                <button
-                    className="rounded-lg bg-primary px-8 py-2 text-black"
-                    onClick={gotoHomePage}
-                >
-                    Go to HomePage
-                </button>
+                <div className="flex flex-wrap justify-center gap-4">
+                    <button
+                        className="mr-4 rounded-lg bg-primary px-8 py-2 text-black"
+                        onClick={reloadPage}
+                    >
+                        Try Again
+                    </button>
+                    <button
+                        className="rounded-lg bg-primary px-8 py-2 text-black"
+                        onClick={gotoHomePage}
+                    >
+                        Go to HomePage
+                    </button>
+                </div>
             )}
         </div>
     )
