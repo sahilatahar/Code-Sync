ea9d6ee5319f819e2cafa16d12a1c5d71bff2afd
Sahil Atahar <sahilatahar@gmail.com>
2024-02-19 19:56:08 +0530

    improve performance and seo

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,17 +1,34 @@
+import { useEffect, useState } from "react"
 import illustration from "../assets/illustration.svg"
 import FormComponent from "../components/forms/FormComponent"
 // import Footer from "../components/common/Footer";
 
 function HomePage() {
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        const loadImage = async () => {
+            await new Promise((resolve) => {
+                const img = new Image()
+                img.onload = () => resolve(img)
+                img.src = illustration
+            })
+            setIsLoaded(true)
+        }
+        loadImage()
+    }, [])
+
     return (
         <div className="flex min-h-screen flex-col items-center justify-center gap-16">
             <div className="flex h-full min-w-full flex-col items-center justify-evenly pt-12 sm:flex-row sm:pt-0">
                 <div className="flex w-full animate-up-down justify-center sm:w-auto sm:pl-4">
-                    <img
-                        src={illustration}
-                        alt=""
-                        className="mx-auto w-[250px] sm:w-[400px]"
-                    />
+                    {isLoaded && (
+                        <img
+                            src={illustration}
+                            alt="Code Sync Illustration"
+                            className="mx-auto w-[250px] sm:w-[400px]"
+                        />
+                    )}
                 </div>
                 <FormComponent />
             </div>
