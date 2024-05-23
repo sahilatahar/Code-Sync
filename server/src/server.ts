4231570ae9545537f883803f6249edbee99a46ee
Sahil Atahar <sahilatahar@gmail.com>
2024-05-23 14:11:15 +0530

    🚀✨ Migrate Server to TypeScript

---
+++ server/src/server.ts
@@ -0,0 +1,222 @@
+import express, { Response, Request } from "express"
+import dotenv from "dotenv"
+import http from "http"
+import cors from "cors"
+import { MessageEvent, SocketId } from "./types/socket"
+import { USER_CONNECTION_STATUS, User } from "./types/user"
+import { Server } from "socket.io"
+import path from "path"
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+app.use(express.static(path.join(__dirname, "public"))) // Serve static files
+
+const server = http.createServer(app)
+const io = new Server(server, {
+	cors: {
+		origin: "*",
+	},
+})
+
+let userSocketMap: User[] = []
+
+// Function to get all users in a room
+function getUsersInRoom(roomId: string): User[] {
+	return userSocketMap.filter((user) => user.roomId == roomId)
+}
+
+// Function to get room id by socket id
+function getRoomId(socketId: SocketId): string | null {
+	const roomId = userSocketMap.find(
+		(user) => user.socketId === socketId
+	)?.roomId
+
+	if (!roomId) {
+		console.error("Room ID is undefined for socket ID:", socketId)
+		return null
+	}
+	return roomId
+}
+
+function getUserBySocketId(socketId: SocketId): User | null {
+	const user = userSocketMap.find((user) => user.socketId === socketId)
+	if (!user) {
+		console.error("User not found for socket ID:", socketId)
+		return null
+	}
+	return user
+}
+
+io.on("connection", (socket) => {
+	// Handle user actions
+	socket.on(MessageEvent.JOIN_REQUEST, ({ roomId, username }) => {
+		// Check is username exist in the room
+		const isUsernameExist = getUsersInRoom(roomId).filter(
+			(u) => u.username === username
+		)
+		if (isUsernameExist.length > 0) {
+			io.to(socket.id).emit(MessageEvent.USERNAME_EXISTS)
+			return
+		}
+
+		const user = {
+			username,
+			roomId,
+			status: USER_CONNECTION_STATUS.ONLINE,
+			cursorPosition: 0,
+			typing: false,
+			socketId: socket.id,
+			currentFile: null,
+		}
+		userSocketMap.push(user)
+		socket.join(roomId)
+		socket.broadcast.to(roomId).emit(MessageEvent.USER_JOINED, { user })
+		const users = getUsersInRoom(roomId)
+		io.to(socket.id).emit(MessageEvent.JOIN_ACCEPTED, { user, users })
+	})
+
+	socket.on("disconnecting", () => {
+		const user = userSocketMap.find((user) => user.socketId === socket.id)
+		const roomId = user?.roomId
+		if (roomId === undefined || user === undefined) return
+		socket.broadcast
+			.to(roomId)
+			.emit(MessageEvent.USER_DISCONNECTED, { user })
+		userSocketMap = userSocketMap.filter((u) => u.socketId !== socket.id)
+		socket.leave(roomId)
+	})
+
+	// Handle file actions
+	socket.on(MessageEvent.SYNC_FILES, ({ files, currentFile, socketId }) => {
+		io.to(socketId).emit(MessageEvent.SYNC_FILES, {
+			files,
+			currentFile,
+		})
+	})
+
+	socket.on(MessageEvent.FILE_CREATED, ({ file }) => {
+		const roomId = getRoomId(socket.id)
+		if (!roomId) return
+		socket.broadcast.to(roomId).emit(MessageEvent.FILE_CREATED, { file })
+	})
+
+	socket.on(MessageEvent.FILE_UPDATED, ({ file }) => {
+		const roomId = getRoomId(socket.id)
+		if (!roomId) return
+		socket.broadcast.to(roomId).emit(MessageEvent.FILE_UPDATED, { file })
+	})
+
+	socket.on(MessageEvent.FILE_RENAMED, ({ file }) => {
+		const roomId = getRoomId(socket.id)
+		if (!roomId) return
+		socket.broadcast.to(roomId).emit(MessageEvent.FILE_RENAMED, { file })
+	})
+
+	socket.on(MessageEvent.FILE_DELETED, ({ id }) => {
+		const roomId = getRoomId(socket.id)
+		if (!roomId) return
+		socket.broadcast.to(roomId).emit(MessageEvent.FILE_DELETED, { id })
+	})
+
+	// Handle user status
+	socket.on(MessageEvent.USER_OFFLINE, ({ socketId }) => {
+		userSocketMap = userSocketMap.map((user) => {
+			if (user.socketId === socketId) {
+				return { ...user, status: USER_CONNECTION_STATUS.OFFLINE }
+			}
+			return user
+		})
+		const roomId = getRoomId(socketId)
+		if (!roomId) return
+		socket.broadcast
+			.to(roomId)
+			.emit(MessageEvent.USER_OFFLINE, { socketId })
+	})
+
+	socket.on(MessageEvent.USER_ONLINE, ({ socketId }) => {
+		userSocketMap = userSocketMap.map((user) => {
+			if (user.socketId === socketId) {
+				return { ...user, status: USER_CONNECTION_STATUS.ONLINE }
+			}
+			return user
+		})
+		const roomId = getRoomId(socketId)
+		if (!roomId) return
+		socket.broadcast.to(roomId).emit(MessageEvent.USER_ONLINE, { socketId })
+	})
+
+	// Handle chat actions
+	socket.on(MessageEvent.SEND_MESSAGE, ({ message }) => {
+		const roomId = getRoomId(socket.id)
+		if (!roomId) return
+		socket.broadcast
+			.to(roomId)
+			.emit(MessageEvent.RECEIVE_MESSAGE, { message })
+	})
+
+	// Handle cursor position
+	socket.on(MessageEvent.TYPING_START, ({ cursorPosition }) => {
+		userSocketMap = userSocketMap.map((user) => {
+			if (user.socketId === socket.id) {
+				return { ...user, typing: true, cursorPosition }
+			}
+			return user
+		})
+		const user = getUserBySocketId(socket.id)
+		if (!user) return
+		const roomId = user.roomId
+		socket.broadcast.to(roomId).emit(MessageEvent.TYPING_START, { user })
+	})
+
+	socket.on(MessageEvent.TYPING_PAUSE, () => {
+		userSocketMap = userSocketMap.map((user) => {
+			if (user.socketId === socket.id) {
+				return { ...user, typing: false }
+			}
+			return user
+		})
+		const user = getUserBySocketId(socket.id)
+		if (!user) return
+		const roomId = user.roomId
+		socket.broadcast.to(roomId).emit(MessageEvent.TYPING_PAUSE, { user })
+	})
+
+	socket.on(MessageEvent.REQUEST_DRAWING, () => {
+		const roomId = getRoomId(socket.id)
+		if (!roomId) return
+		socket.broadcast
+			.to(roomId)
+			.emit(MessageEvent.REQUEST_DRAWING, { socketId: socket.id })
+	})
+
+	socket.on(MessageEvent.SYNC_DRAWING, ({ drawingData, socketId }) => {
+		socket.broadcast
+			.to(socketId)
+			.emit(MessageEvent.SYNC_DRAWING, { drawingData })
+	})
+
+	socket.on(MessageEvent.DRAWING_UPDATE, ({ snapshot }) => {
+		const roomId = getRoomId(socket.id)
+		if (!roomId) return
+		socket.broadcast.to(roomId).emit(MessageEvent.DRAWING_UPDATE, {
+			snapshot,
+		})
+	})
+})
+
+const PORT = process.env.PORT || 3000
+
+app.get("/", (req: Request, res: Response) => {
+	// Send the index.html file
+	res.sendFile(path.join(__dirname, "..", "public", "index.html"))
+})
+
+server.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`)
+})
