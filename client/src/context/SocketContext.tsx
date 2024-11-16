d417f9e3c938f63f1d1d3cb824b07d03c20841cf
Vansh <86408597+vansh1505@users.noreply.github.com>
2024-11-17 02:41:19 +0530

    Now supported local hosting

--- client/src/context/SocketContext.tsx
+++ client/src/context/SocketContext.tsx
@@ -27,7 +27,7 @@ export const useSocket = (): SocketContextType => {
     return context
 }
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
 
 const SocketProvider = ({ children }: { children: ReactNode }) => {
     const {
