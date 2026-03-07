61508899a7df025cb3ef2d74748687e77d8e48ee
Your Name <you@example.com>
2026-03-08 00:44:46 +0300

    Configurable Piston API URL

--- client/src/api/pistonApi.ts
+++ client/src/api/pistonApi.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios"
 
-const pistonBaseUrl = "https://emkc.org/api/v2/piston"
+const pistonBaseUrl = import.meta.env.VITE_PISTON_API_URL || "http://localhost:2000/api/v2"
 
 const instance: AxiosInstance = axios.create({
     baseURL: pistonBaseUrl,
