072d0b1ce5de88f7d3e2baa36136933b784edc62
Your Name <you@example.com>
2026-05-29 15:13:00 +0300

    Configurable interface colors

--- client/src/config/defaults.ts
+++ client/src/config/defaults.ts
@@ -13,6 +13,13 @@ const config = {
 	defaultFontSize: 16,
 	defaultFontFamily: 'Space Mono',
 	defaultShowGitHubCorner: true,
+	interfaceColors: {
+		dark: "#212429",
+		darkHover: "#3D404A",
+		light: "#f5f5f5",
+		primary: "#39E079",
+		danger: "#ef4444",
+	},
 }
 
 export default config
