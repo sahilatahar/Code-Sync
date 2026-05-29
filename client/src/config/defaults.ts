75a2b9ed6c14f0f026efc2dc10db2f05a85f02a2
Your Name <you@example.com>
2026-05-30 02:02:54 +0300

    Configurable sidebar tabs

--- client/src/config/defaults.ts
+++ client/src/config/defaults.ts
@@ -1,3 +1,5 @@
+import { VIEWS } from "../types/view"
+
 const defaultFileContent = `function sayHi() {
   console.log("👋 Hello world");
 }
@@ -6,13 +8,18 @@ sayHi()`;
 const config = {
 	backendUrl: 'http://localhost:3000',
 	pistonApiUrl: 'http://localhost:2000/api/v2',
+
 	defaultFileName: 'index.js',
 	defaultFileContent,
 	defaultProjectLanguage: 'Javascript',
+
 	defaultTheme: 'Dracula',
 	defaultFontSize: 16,
 	defaultFontFamily: 'Space Mono',
 	defaultShowGitHubCorner: true,
+
+	visibleTabs: [VIEWS.FILES, VIEWS.CHATS, VIEWS.COPILOT, VIEWS.RUN, VIEWS.CLIENTS, VIEWS.SETTINGS],
+	showDrawingToggle: true,
 	interfaceColors: {
 		dark: "#212429",
 		darkHover: "#3D404A",
